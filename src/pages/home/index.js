64ec7af6823b78947cc278697b1753b014d69a69
ceciliaramoscavazos <aceciliaramos@gmail.com>
2023-10-23 21:40:33 -0600

    set button to change theme

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React , {useState} from "react";
 
 import "./home.scss"
 
 function Home(){
+    const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+    const toggleTheme = () => {
+        setIsDarkTheme(!isDarkTheme)
+    }
     return (
-        <section className="home dark-theme">
+        <section className={`home ${isDarkTheme ? 'dark-theme' : 'light-theme'}`}>
             <h1>Home</h1>
+            <button onClick={toggleTheme}>Toggle Theme</button>
         </section>
         )
 }
