fd5969e561cd4f6f1abdcf3b43c2789fd6e0c466
ceciliaramoscavazos <aceciliaramos@gmail.com>
2023-10-23 21:02:13 -0600

    clase en section

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,15 +3,11 @@ import React from "react";
 import "./home.scss"
 
 function Home(){
-    return (<div className="home">
-        <h1>Estilo home</h1>
-        <div className="container">
-            <h1>Estilo container</h1>
-            <div className="wrapper">
-            <h1>Estilo wrapper</h1>
-            </div>
-        </div>
-    </div>) 
+    return (
+        <section className="home">
+            <h1>Home</h1>
+        </section>
+        )
 }
 
 export default Home
