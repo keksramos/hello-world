56c5fcc134daf3a07c40bbd75a7af98048e2bda9
ceciliaramoscavazos <aceciliaramos@gmail.com>
2023-10-27 18:29:29 -0600

    no cambios

--- src/components/PokemonBrowser/PokemonSearch.js
+++ src/components/PokemonBrowser/PokemonSearch.js
@@ -2,37 +2,41 @@ import React, { useState } from "react";
 import Pokemon from "../Pokemon/Pokemon";
 
 export default function PokemonSearch() {
-    const [pokemon, setPokemon] = useState({});
-    
-    const searchPokemon = async() => {
-        const pokemonName = document.getElementById('pokemon-name').value
-        const data = await(await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)).json()
+  const [pokemon, setPokemon] = useState({});
 
-        setPokemon({
-            id: data.id,
-            name: data.name,
-            sprite: data.sprites.front_default,
-            types: data.types.map((type) => {
-                return type.type.name
-            })
-        });
-        }
+  const searchPokemon = async () => {
+    const pokemonName = document.getElementById("pokemon-name").value;
+    const data = await (
+      await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`
+      )
+    ).json();
 
-    return (
-       <div className="pokemon-search">
-        {
-            pokemon.id ? (
-                <Pokemon {...pokemon} />
-        ) : (
-            <div>
-                <h1>Pokemon Search</h1>
-                <input type="text" placeholder="Search a pokemon" id="pokemon-name"></input>
-                <button onClick={searchPokemon}>Search</button>
-                {pokemon.id && <Pokemon {...pokemon} />}
-            </div> 
-        )
-    }
-    </div>
-    )
+    setPokemon({
+      id: data.id,
+      name: data.name,
+      sprite: data.sprites.front_default,
+      types: data.types.map((type) => {
+        return type.type.name;
+      }),
+    });
+  };
+  return (
+    <div className="pokemon-search">
+     {
+         pokemon.id ? (
+             <Pokemon {...pokemon} />
+     ) : (
+         <div>
+             <h1>Pokemon Search</h1>
+             <input type="text" placeholder="Search a pokemon" id="pokemon-name"></input>
+             <button onClick={searchPokemon}>Search</button>
+             {pokemon.id && <Pokemon {...pokemon} />}
+         </div>
+     )
+ }
+ </div>
+ )
 }
+
 
