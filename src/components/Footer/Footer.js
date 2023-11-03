cd993180792de06256ce77d44fba841b7c4a8b9e
ceciliaramoscavazos <aceciliaramos@gmail.com>
2023-11-02 22:27:15 -0600

    footer finished

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,20 @@
 import React from "react";
-import "../Footer/footer.scss"
+import "../Footer/footer.scss";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="contentainerFooter">
+      <div className="ContainerFooter">
+            <p className="titulosFooter">Páginas</p>
         <div className="links">
-          <Link to="/">Home</Link>
-          <Link to="/episodes">Episodes</Link>
-          <Link to="/pokemones">Pokemones</Link>
+            <Link to="/">Home</Link>
+            <Link to="/episodes">Episodes</Link>
+            <Link to="/pokemones">Pokemones</Link>
         </div>
+      <div className="copyright">
+      Cecilia Ramos - 2023
+      </div>
       </div>
     </footer>
   );
