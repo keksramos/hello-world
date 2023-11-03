07f6125dc0a0f7ca2c613a88744c615f95b39e23
ceciliaramoscavazos <aceciliaramos@gmail.com>
2023-11-02 19:22:49 -0600

    instalamos route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,62 +6,18 @@ import Box from '../Box/Box';
 import Home from '../../pages/home';
 import Episodes from '../../pages/episodes';
 import Pokemones from '../../pages/pokemones';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const GradesContext = React.createContext()
 
 function App() {
-
-  const [usersWithGrades, setUsersWithGrades] = useState([
-    {
-      name: 'Cecilia',
-      grade: 10,
-    },
-    {
-      name: 'Brenda',
-      grade: 9,
-    },
-    {
-      name: 'Sofia',
-      grade: 8.5,
-    },
-    {
-      name: 'Elena',
-      grade: 10,
-    },
-    {
-      name: 'Julieta',
-      grade: 9.5,
-    },
-    {
-      name: 'Iñigo',
-      grade: 9.3,
-    },
-    {
-      name: 'Julio',
-      grade:7.5,
-    },
-    {
-      name: 'Miguel',
-      grade: 9.5,
-    }
-  ])
-
-  const [activePage, setActivePage] = useState('episodes')
-
-  return(
-  <GradesContext.Provider value = {{usersWithGrades, setUsersWithGrades}}>
-    {
-      activePage === 'home' ? <Home /> : <Episodes/>
-    }
-    </GradesContext.Provider>
-    )
-
-    // codigo de tarea
-    // return(
-    //   <GradesContext.Provider value = {{usersWithGrades, setUsersWithGrades}}>
-    //     <Home />
-    //     </GradesContext.Provider>
-    //     )
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Episodes />} />
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
